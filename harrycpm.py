9cace3beb8f9ae4a4a43cd3beb0a88ec46144014
CPMRm <rmcpmstore@gmail.com>
2025-05-09 21:33:50 +0800

    Update harrycpm.py

--- harrycpm.py
+++ harrycpm.py
@@ -383,9 +383,10 @@ class HarryCPM:
         response_decoded = response.json()
         return response_decoded.get("ok")
 
-    def incline(self, car_id, custom): 
-        payload = { "account_auth": self.auth_token, "car_id": car_id, "custom": custom } 
-        params = { "key": self.access_key, "car_id": car_id, "custom": custom } 
-        response = requests.post(f"{ENDPOINT_URL}/incline", params=params, data=payload) 
-        response_decoded = response.json()         
+    def incline(self, car_id, custom):
+        payload = { "account_auth": self.auth_token, "car_id": car_id, "custom": custom }
+        params = { "key": self.access_key, "car_id": car_id, "custom": custom }
+        response = requests.post(f"{__ENDPOINT_URL__}/incline", params=params, data=payload)
+        response_decoded = response.json()
+        self.log_action("stance_camber", { "payload": payload, "params": params })
         return response_decoded.get("ok")
